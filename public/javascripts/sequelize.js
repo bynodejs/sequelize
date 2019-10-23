3714889b707190aab0725cdd48c2d0faee8832d0
임경민 <ljlm0402@naver.com>
2019-10-23 17:11:21 +0900

    fix

--- public/javascripts/sequelize.js
+++ public/javascripts/sequelize.js
@@ -5,7 +5,6 @@ document.querySelectorAll("#user-list tr").forEach(function(el) {
     getComment(id);
   });
 });
-
 // 사용자 로딩
 function getUser() {
   var xhr = new XMLHttpRequest();
@@ -40,7 +39,6 @@ function getUser() {
   xhr.open("GET", "/users");
   xhr.send();
 }
-
 // 댓글 로딩
 function getComment(id) {
   var xhr = new XMLHttpRequest();
@@ -84,6 +82,10 @@ function getComment(id) {
         remove.textContent = "삭제";
         remove.addEventListener("click", function() {
           // 삭제 클릭 시
+          var newConfirm = confirm("삭제 하시겠습니까?");
+          if (!newConfirm) {
+            return alert("삭제가 취소되었습니다.");
+          }
           var xhr = new XMLHttpRequest();
           xhr.onload = function() {
             if (xhr.status === 200) {
@@ -110,7 +112,6 @@ function getComment(id) {
   xhr.open("GET", "/comments/" + id);
   xhr.send();
 }
-
 // 사용자 등록 시
 document.getElementById("user-form").addEventListener("submit", function(e) {
   e.preventDefault();
@@ -138,7 +139,6 @@ document.getElementById("user-form").addEventListener("submit", function(e) {
   e.target.age.value = "";
   e.target.married.checked = false;
 });
-
 // 댓글 등록 시
 document.getElementById("comment-form").addEventListener("submit", function(e) {
   e.preventDefault();
